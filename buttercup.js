4caaa7f503064cf4f65719b15b3ff60e8515a337
Nyala Jackson <nyalajackson@yahoo.com>
2016-05-19 21:44:58 +0000

    almost there

--- buttercup.js
+++ buttercup.js
@@ -4,4 +4,16 @@ $(document).ready(function() {
         $("#chatbox").append(text+"\n");
         $("#enter").val("");
     });
+    $("#gameOne").click(function(){
+        window.location = "https://www.youtube.com";
+    });
+    $("#gameTwo").click(function(){
+       window.location = "https://www.facebook.com";
+    });
+    $("#gameThree").click(function(){
+       window.location = "https://www.tumblr.com";
+    });
+    $("#showcase").hover(function(){
+        $("#intro").css({'color':'white'});
+    });
 });
