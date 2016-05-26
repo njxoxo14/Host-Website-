bb3e0f1d40027f974bd5dad3f3b8cae68cdb302f
Nyala Jackson <nyalajackson@yahoo.com>
2016-05-26 10:21:57 +0000

    I don't think it's good enough...

--- buttercup.js
+++ buttercup.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
     $("#chatsubmit").click(function() {
         var text = $("#enter").val();
-        $("#chatbox").append(text+"\n");
+        $("#chatbox").append(text + "\n");
         $("#enter").val("");
     });
-    $("#gameOne").click(function(){
+    $("#chat").keyup(function(event) {
+        if (event.keyCode == 13) {
+            $("#chatsubmit").click();
+        }
+    });
+    $("#gameOne").click(function() {
         window.location = "https://www.youtube.com";
     });
-    $("#gameTwo").click(function(){
-       window.location = "https://www.facebook.com";
+    $("#gameTwo").click(function() {
+        window.location = "https://www.facebook.com";
     });
-    $("#gameThree").click(function(){
-       window.location = "https://www.tumblr.com";
+    $("#gameThree").click(function() {
+        window.location = "https://www.tumblr.com";
     });
-    $("#showcase").hover(function(){
-        $("#intro").css({'color':'white'});
+    $(".dropdown-content").click(function(){
+       window.location= "http://output.jsbin.com/nipop" 
     });
+   
 });
